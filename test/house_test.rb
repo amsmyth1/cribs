2ae72672dc298c542522645bd402a493365f2c3b
Alexa Morales <alexamsmyth@gmail.com>
2020-12-09 13:20:47 -0500

    Create house class and tests

---
+++ test/house_test.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+require './lib/house'
+
+class RoomTest < Minitest::Test
+  def test_it_exists
+    room = Room.new(:bedroom, 10, '13')
+    house = House.new("$400000", "123 sugar lane")
+    assert_instance_of House, house
+  end
+
+  def test_it_has_readable_attributes
+    room = Room.new(:bedroom, 10, '13')
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal [], house.rooms
+    assert_equal "$400000", house.price
+    assert_equal "123 sugar lane", house.address
+  end
+
+  def test_add_room
+    room = Room.new(:bedroom, 10, '13')
+    house = House.new("$400000", "123 sugar lane")
+    house.add_room(room)
+
+    assert_equal [room], house.rooms
+
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room_2)
+
+    assert_equal [room, room_2], house.rooms 
+  end
+end
