ca559fa0f848dcd6db0da07ac6f36930ded497e7
Alexa Morales <alexamsmyth@gmail.com>
2020-12-09 13:11:40 -0500

    Create room class and tests

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,26 @@
-class Rom
+class Room
+  attr_reader :category,
+              :length,
+              :width,
+              :paint
 
   def initialize(category, length, width)
     @category = category
-    @length = 10
+    @length = length
     @width = width
+    @paint = false
   end
 
   def area
-    @length * @width
+    @length * @width.to_i
   end
+
+  def is_painted?
+    @paint
+  end
+
+  def paint
+    @paint = true
+  end 
+
 end
